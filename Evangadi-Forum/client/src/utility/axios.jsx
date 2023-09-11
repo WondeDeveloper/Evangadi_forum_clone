d310934a5c6fef7dbc0dd401f86d7c1036a9a6d5
WondeDeveloper <135033144+WondeDeveloper@users.noreply.github.com>
2023-09-11 18:11:50 -0400

    axios

--- Evangadi-Forum/client/src/utility/axios.jsx
+++ Evangadi-Forum/client/src/utility/axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
 	// baseURL: "http://localhost:4500", // api url
-	baseURL: "https://determined-tuna-panama-hat.cyclic.app", // api url
+	//baseURL: "https://determined-tuna-panama-hat.cyclic.app",  // api url
+	baseURL: "https://evangadiforum-server-render.onrender.com",
 
 	withCredentials: true,
 });
