4ae01fa2a2642180d34bdddea34aa2fdbdc556ef
WondeDeveloper <135033144+WondeDeveloper@users.noreply.github.com>
2023-09-13 11:51:09 -0400

    minor fix

--- Evangadi-Forum/client/src/utility/axios.jsx
+++ Evangadi-Forum/client/src/utility/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	// baseURL: "http://localhost:4500", // api url
+	//baseURL: "http://localhost:4500", // api url
 	//baseURL: "https://determined-tuna-panama-hat.cyclic.app",  // api url
 	baseURL: "https://evangadiforum-server-render.onrender.com",
 
